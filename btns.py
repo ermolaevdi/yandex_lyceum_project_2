ab9fd01890b64a29c1cba48113a58af7f3d10ed6
ermolaevdi <153669288+ermolaevdi@users.noreply.github.com>
2024-01-09 23:43:29 +0300

    Update btns.py

--- btns.py
+++ btns.py
@@ -1,5 +1,6 @@
 import pygame
 
+from font import Font
 from tuning import *
 
 
@@ -8,31 +9,29 @@ class Buttons:
     def __init__(self):
 
         self.buttons = []
-        #####ДОБАВИТЬ ЦВЕТА - font
-        #####ДОБАВИТЬ ЭСКИЗЫ - 1.png и 2.png
-        #####font = Font()
+        
+        font = Font()
 
-        #self.buttons.append(Button(Buttons.CHECK, 780, , ["1.png", "2.png"], "Проверить правильность решения? Помни, "
-        #                                                             "что счётчик ошибок может увеличиться :)", font))
-        #self.buttons.append(Button(Buttons.RESTART, 780, , ["1.png", "2.png"], "Начать прохождение уровня заново", font))
-        #self.buttons.append(Button(Buttons.ADVICE, 780, , ["1.png", "2.png"], "Открыть любые 1-3 клетки", font))
+        self.buttons.append(Button(Buttons.CHECK, 780, 110, ["4.png", "3.png"], "Проверить правильность решения? Помни, что счётчик ошибок может увеличиться :)", font))
+        self.buttons.append(Button(Buttons.RESTART, 780, 230, ["24.png", "23.png"], "Начать прохождение уровня заново", font))
+        self.buttons.append(Button(Buttons.ADVICE, 780, 290, ["8.png", "7.png"], "Открыть любые 1-3 клетки", font))
 
-        #self.buttons.append(Button(Buttons.NEXT, 780, , ["1.png", "2.png"], "Следующий уровень", font))
-        #self.buttons.append(Button(Buttons.PREV, 780, 430, ["1.png", "2.png"], "Предыдущий уровень", font))
-        #self.buttons.append(Button(Buttons.RULES, 780, , ["1.png", "2.png"], "Правила игры", font))
-        #self.buttons.append(Button(Buttons.EXIT, 780, 470, ["1.png", "2.png"], "Выйти из головоломки", font))
-        #self.buttons.append(Button(Buttons.RESET, 780, 500, ["1.png", "2.png"], "Сбросить весь прогресс", font))
+        self.buttons.append(Button(Buttons.NEXT, 780, 440, ["16.png", "15.png"], "Следующий уровень", font))
+        self.buttons.append(Button(Buttons.PREV, 780, 470, ["18.png", "17.png"], "Предыдущий уровень", font))
+        self.buttons.append(Button(Buttons.RULES, 780, 530, ["20.png", "19.png"], "Правила игры", font))
+        self.buttons.append(Button(Buttons.EXIT, 780, 590, ["6.png", "5.png"], "Выйти из головоломки", font))
+        self.buttons.append(Button(Buttons.RESET, 780, 620, ["22.png", "21.png"], "Сбросить весь прогресс", font))
 
         # Кнопки для выставления сложности
-        #self.buttons_difficulty = []
-        #self.buttons_difficulty.append(Button(Buttons.D30, 780, 310, ["1.png", "2.png"],
-        #                                      "Меньше половины чисел скрыто выражениями", font))
-        #self.buttons_difficulty.append(Button(Buttons.D60, 780, 310, ["1.png", "2.png"],
-        #                                      "Больше половины чисел скрыто выражениями", font))
-        #self.buttons_difficulty.append(Button(Buttons.D100, 780, 310, ["1.png", "2.png"],
-        #                                      "Все числа скрыты выражениями", font))
+        self.buttons_difficulty = []
+        self.buttons_difficulty.append(Button(Buttons.D30, 780, 320, ["10.png", "9.png"],
+                                              "Меньше половины чисел скрыто выражениями", font))
+        self.buttons_difficulty.append(Button(Buttons.D60, 780, 320, ["12.png", "11.png"],
+                                              "Больше половины чисел скрыто выражениями", font))
+        self.buttons_difficulty.append(Button(Buttons.D100, 780, 320, ["14.png", "13.png"],
+                                              "Все числа скрыты выражениями", font))
 
-        #self.buttons.append(Button(Buttons.AUTHOR, 795, HEIGHT - 40, ["1.png", "2.png"], "Разработчик игры", font))
+        self.buttons.append(Button(Buttons.AUTHOR, 780, 740, ["2.png", "1.png"], "Разработчик игры", font))
 
     # Номера кнопок
     RULES = 0
@@ -91,7 +90,7 @@ class Button:
         self.demonstrable = True
 
         for file in im_name_file:
-            self.images.append(pygame.image.load(f"png/{file}"))
+            self.images.append(pygame.image.load(f"foto_png/{file}"))
 
         self.w = self.images[0].get_width()
         self.h = self.images[0].get_height()
@@ -116,10 +115,11 @@ class Button:
         return "NONE"
 
     # Появление внизу подсказки при наведении курсора на кнопку
-    #def write_advice(self, scene):
-        #sc = self.font.getSystemText(f"N{self.y}", f"{self.advice}", #####ЦВЕТ)
-        #x = (WIDTH - sc.get_width()) // 2
-        #scene.blit(sc, (x, HEIGHT - 40))
+    def write_advice(self, scene):
+        sc = self.font.getSystemText(f"N{self.y}", f"{self.hint}", COLOR_YELLOW_DARK)
+        x = (WIDTH - sc.get_width()) // 2
+        scene.blit(sc, (x, 80))
+        ######scene.blit(sc, (x, HEIGHT - 40))
 
     # Местонахождение мыши по отношению к кнопке
     def get_mouse_btn(self, x, y):
