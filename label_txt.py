c34ab3186014b108cff639bf46f6dbf0cf972128
ermolaevdi <153669288+ermolaevdi@users.noreply.github.com>
2024-01-19 13:34:58 +0300

    Update label_txt.py

--- label_txt.py
+++ label_txt.py
@@ -12,8 +12,9 @@ class LabelText:
         scene.blit(self.font.getSystemText("ERROR", f"Ошибки: {error}",
                                            TEXT_COLOR[0]), (10, 10))
         scene.blit(self.font.getSystemText("LEVEL", f"Головоломка: {level + 1}/{max_level + 1}",
-                                           TEXT_COLOR[0]), (210, 10))
+                                           TEXT_COLOR[0]), (200, 10))
+        #print(max_level + 1)
         scene.blit(self.font.getSystemText("ADVICE", f"Подсказки: {advice}",
-                                           TEXT_COLOR[0]), (440, 10))
+                                           TEXT_COLOR[0]), (480, 10))
         scene.blit(self.font.getSystemText("MAX_LEVEL", f"Пройдено уровней: {max_level}",
-                                           TEXT_COLOR[0]), (660, 10))
+                                           TEXT_COLOR[0]), (710, 10))
