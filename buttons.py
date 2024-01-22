a8fd63c195258e52a96961015df1b3dc435b5837
ermolaevdi <153669288+ermolaevdi@users.noreply.github.com>
2024-01-22 21:30:08 +0300

    Update and rename btns.py to buttons.py

--- buttons.py
+++ buttons.py
@@ -1,7 +1,7 @@
 import pygame
 
+import tuning
 from font import Font
-from tuning import *
 
 
 class Buttons:
@@ -20,118 +20,116 @@ class Buttons:
     RESET = 10
     AUTHOR = 11
 
-
     def __init__(self):
 
         self.buttons = []
-
         font = Font()
 
-        self.buttons.append(Button(Buttons.CHECK, 780, 150, ["4.png", "3.png"],
-                                   "Проверить правильность решения?"
+        self.buttons.append(Button(Buttons.CHECK, 790, 150, ["4.png", "3.png"],
+                                   "Проверить правильность решения? "
                                    "Помни, что счётчик ошибок может увеличиться :)", font))
-        self.buttons.append(Button(Buttons.RESTART, 780, 230, ["24.png", "23.png"],
+        self.buttons.append(Button(Buttons.RESTART, 790, 230, ["24.png", "23.png"],
                                    "Начать прохождение уровня заново", font))
-        self.buttons.append(Button(Buttons.ADVICE, 780, 290, ["8.png", "7.png"],
+        self.buttons.append(Button(Buttons.ADVICE, 790, 290, ["8.png", "7.png"],
                                    "Открыть любые 1-3 клетки", font))
 
-        self.buttons.append(Button(Buttons.NEXT, 780, 440, ["16.png", "15.png"],
+        self.buttons.append(Button(Buttons.NEXT, 790, 440, ["16.png", "15.png"],
                                    "Следующий уровень", font))
-        self.buttons.append(Button(Buttons.PREV, 780, 470, ["18.png", "17.png"],
+        self.buttons.append(Button(Buttons.PREV, 790, 470, ["18.png", "17.png"],
                                    "Предыдущий уровень", font))
-        self.buttons.append(Button(Buttons.RULES, 780, 530, ["20.png", "19.png"],
+        self.buttons.append(Button(Buttons.RULES, 790, 530, ["20.png", "19.png"],
                                    "Правила игры", font))
-        self.buttons.append(Button(Buttons.EXIT, 780, 570, ["6.png", "5.png"],
+        self.buttons.append(Button(Buttons.EXIT, 790, 570, ["6.png", "5.png"],
                                    "Выйти из головоломки", font))
-        self.buttons.append(Button(Buttons.RESET, 780, 600, ["22.png", "21.png"],
+        self.buttons.append(Button(Buttons.RESET, 790, 600, ["22.png", "21.png"],
                                    "Сбросить весь прогресс", font))
 
         # Кнопки для выставления сложности
         self.buttons_difficulty = []
-        self.buttons_difficulty.append(Button(Buttons.D30, 780, 320, ["10.png", "9.png"],
+        self.buttons_difficulty.append(Button(Buttons.D30, 790, 320, ["10.png", "9.png"],
                                               "Меньше половины чисел скрыто выражениями", font))
-        self.buttons_difficulty.append(Button(Buttons.D60, 780, 320, ["12.png", "11.png"],
+        self.buttons_difficulty.append(Button(Buttons.D60, 790, 320, ["12.png", "11.png"],
                                               "Больше половины чисел скрыто выражениями", font))
-        self.buttons_difficulty.append(Button(Buttons.D100, 780, 320, ["14.png", "13.png"],
+        self.buttons_difficulty.append(Button(Buttons.D100, 790, 320, ["14.png", "13.png"],
                                               "Все числа скрыты выражениями", font))
 
-        self.buttons.append(Button(Buttons.AUTHOR, 780, 640, ["2.png", "1.png"],
+        self.buttons.append(Button(Buttons.AUTHOR, 790, 640, ["2.png", "1.png"],
                                    "Разработчик игры", font))
-
 
     def draw(self, scene, mouse_x, mouse_y, pressable):
         return_id_button = "NONE"
         res = "NONE"
         for btn in self.buttons:
-            if btn.id_button == Buttons.ADVICE and advice > 0 and error < 8:
+            if btn.id_button == Buttons.ADVICE and tuning.advice > 0 and tuning.error < 8:
                 res = btn.draw(scene, mouse_x, mouse_y, pressable)
-            elif btn.id_button == Buttons.NEXT and level < max_level and error < 8:
+            elif btn.id_button == Buttons.NEXT and tuning.level < tuning.max_level and tuning.error < 8:
                 res = btn.draw(scene, mouse_x, mouse_y, pressable)
-            elif btn.id_button == Buttons.PREV and level > 0 and error < 8:
+            elif btn.id_button == Buttons.PREV and tuning.level > 0 and tuning.error < 8:
                 res = btn.draw(scene, mouse_x, mouse_y, pressable)
-            elif btn.id_button != Buttons.ADVICE and btn.id_button != Buttons.NEXT and \
-                 btn.id_button != Buttons.PREV and error < 8:
+            elif btn.id_button != Buttons.ADVICE and \
+                 btn.id_button != Buttons.NEXT and \
+                 btn.id_button != Buttons.PREV and tuning.error < 8:
                 res = btn.draw(scene, mouse_x, mouse_y, pressable)
-            elif error >= 8 and (btn.id_button == Buttons.EXIT or btn.id_button == Buttons.RESET):
+            elif tuning.error >= 8 and (btn.id_button == Buttons.EXIT or btn.id_button == Buttons.RESET):
                 res = btn.draw(scene, mouse_x, mouse_y, pressable)
 
             if res != "NONE":
                 return_id_button = res
 
-        if error < 8:
-            res = self.buttons_difficulty[difficulty].draw(scene, mouse_x, mouse_y, pressable)
+        if tuning.error < 8:
+            res = self.buttons_difficulty[tuning.difficulty].draw(scene, mouse_x, mouse_y, pressable)
 
         if res != "NONE":
             return_id_button = res
 
         return return_id_button
 
-
 class Button:
 
-    def __init__(self, id_btn, x, y, im_name_file, advice_r, font):
-
-        self.id_button = id_btn
+    def __init__(self, id_button, x, y, images_file_name, hint, font):
+        self.id_button = id_button
         self.x = x
         self.y = y
         self.images = []
-        self.advice = advice_r
+        self.hint = hint
         self.font = font
-        self.demonstrable = True
+        self.visible = True
 
-        for file in im_name_file:
+        for file in images_file_name:
             self.images.append(pygame.image.load(f"foto_png/{file}"))
 
-        self.w = self.images[0].get_width()
-        self.h = self.images[0].get_height()
+        self.width = self.images[0].get_width()
+        self.height = self.images[0].get_height()
 
-    def get_mouse_btn(self, x, y):
-        if self.x < x < self.x + self.w and self.y < y < self.y + self.h:
+    def get_mouse_in_button(self, x, y):
+        if self.x < x < self.x + self.width and \
+                self.y < y < self.y + self.height:
             return True
         return False
 
-    def draw(self, scene, mouse_x, mouse_y, pressed):
+    def draw(self, scene, mouse_x, mouse_y, pressable):
         # Номер нажатой кнопки
         pressed_id_button = "NONE"
-        if not self.demonstrable:
+        if not self.visible:
             return pressed_id_button
 
         # Если нажали...
-        if self.get_mouse_btn(mouse_x, mouse_y) and pressed:
+        if self.get_mouse_in_button(mouse_x, mouse_y) and pressable:
             scene.blit(self.images[1], (self.x - 25, self.y))
-            self.write_advice(scene)
+            self.draw_hint(scene)
             pressed_id_button = self.id_button
         else:
             scene.blit(self.images[0], (self.x, self.y))
 
-        if pressed:
+        if pressable:
             return pressed_id_button
 
         return "NONE"
 
     # Появление вверху подсказки при наведении курсора на кнопку
-    def write_advice(self, scene):
-        sc = self.font.getSystemText(f"N{self.y}", f"{self.advice}", COLOR_YELLOW_DARK)
-        x = (WIDTH - sc.get_width()) // 2
+    def draw_hint(self, scene):
+        sc = self.font.getSystemText(f"N{self.y}",
+                                        f"{self.hint}",
+                                        tuning.COLOR_YELLOW_DARK)
+        x = (tuning.WIDTH - sc.get_width()) // 2
         scene.blit(sc, (x, 80))
-        ######scene.blit(sc, (x, HEIGHT - 40))
