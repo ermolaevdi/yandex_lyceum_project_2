be5ae6d2b409c55386337d1f2fbaa45d14002fdd
ermolaevdi <153669288+ermolaevdi@users.noreply.github.com>
2024-01-22 21:34:02 +0300

    Update and rename cells.py to cell.py

--- cell.py
+++ cell.py
@@ -2,23 +2,24 @@ import pygame
 
 import tuning
 from tuning import *
-from sample import Sample
+from view_example import ViewExample
 
 
 class Cell:
     """ Выводим пример для решения, чтобы открыть иконку. """
 
-    def __init__(self, value, x, y, size_square, font, text_clr):
+    def __init__(self, value, x, y, size_field, font, text_color):
 
         self.value = value
+        self.font = font
+        self.text_color = text_color
         self.x = x
         self.y = y
-        self.size_square = size_square
-        self.font = font
-        self.text_clr = text_clr
+        self.size_field = size_field
 
         self.error_digit = False
         self.background = False
+
         self.screen = f"{value}"
 
     def set_error_digit(self):
@@ -28,28 +29,26 @@ class Cell:
     def draw(self, scene: pygame.surface):
         key = f"cell_{self.x}{self.y}"
 
-        if self.screen == "?":
+        if self.screen == "?" :
             pygame.draw.rect(scene, TEXT_LIGHT_ATTENTION,
-                             (self.x + (self.size_square - self.size_square // 1.5) // 2,
-                              self.y, self.size_square // 1.5, 21))
-
+                             (self.x + (self.size_field - self.size_field // 1.5) // 2 , self.y,
+                              self.size_field // 1.5, 21))
             res = self.font.getSystemText(key, f"{self.screen}", COLOR_DEEP_GRAY)
-            coordinates_x0 = (self.size_square - res.get_width()) // 2
-
+            coordinates_x0 = (self.size_field - res.get_width()) // 2
         else:
-            res = self.font.getSystemText(key, f"{self.screen}", self.text_clr)
-            coordinates_x0 = (self.size_square - res.get_width()) // 2
+            res = self.font.getSystemText(key, f"{self.screen}", self.text_color)
+            coordinates_x0 = (self.size_field - res.get_width()) // 2
 
         if self.background:
             if self.error_digit:
-                pygame.draw.rect(scene, TEXT_LIGHT_BAD, (self.x, self.y, self.size_square, 21))
+                pygame.draw.rect(scene, TEXT_LIGHT_BAD, (self.x, self.y, self.size_field, 21))
             else:
-                pygame.draw.rect(scene, TEXT_LIGHT_GOOD, (self.x, self.y, self.size_square, 21))
+                pygame.draw.rect(scene, TEXT_LIGHT_GOOD, (self.x, self.y, self.size_field, 21))
 
         scene.blit(res, (self.x + coordinates_x0, self.y))
 
     def check_mouse(self, x, y):
-        if (self.x < x) and (x < self.x + self.size_square) and (self.y < y) and (y < self.y + 31):
+        if (self.x < x < self.x + self.size_field) and (self.y < y < self.y + 31):
             self.background = True
             return True
         else:
@@ -57,12 +56,11 @@ class Cell:
         return False
 
     def press_mouse_1(self, x, y):
-        if self.screen != "?" and not self.error_digit:
+        if (self.screen != "?") and (self.error_digit == False):
             return False
-
         if self.background:
             if tuning.see_example is None:
-                tuning.see_example = Sample(self.x, self.y, self.size_square, self.value, self)
+                tuning.see_example = ViewExample(self.x, self.y, self.size_field, self.value, self)
 
     def delete_view_example(self):
-        tuning.view_example = None
+        tuning.see_example = None
